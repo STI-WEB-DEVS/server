9b10587531717cf519cdebc4a511ab63800eae40
CrzyBenz <ehillabenz20@gmail.com>
2026-03-07 17:59:31 +0800

    commit

--- routes/api.php
+++ routes/api.php
@@ -3,16 +3,25 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CompanyController;
 use App\Http\Controllers\LanguageController;
+use App\Http\Controllers\ProductsController;
+use App\Http\Controllers\CustomersController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\OrderItemController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login']);
-
-Route::middleware('auth:sanctum')->group(function () {
-    Route::delete('/logout', [AuthController::class, 'logout']);
-
     Route::apiResources([
         'companies' => CompanyController::class,
         'languages' => LanguageController::class,
+        'products' => ProductsController::class,
+        'customers' => CustomersController::class,
+        'Order' => OrderController::class,
+        'OrderItem' => OrderItemController::class,
     ]);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::delete('/logout', [AuthController::class, 'logout']);
+
+
+    
 });
