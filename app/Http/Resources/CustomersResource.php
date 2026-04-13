81ddc6c699a459790dd8dbc7f16895ceb18d7530
Jericca <jericcamaxeneoracion7@gmail.com>
2026-04-13 15:55:56 +0800

    Update customer payload

--- app/Http/Resources/CustomersResource.php
+++ app/Http/Resources/CustomersResource.php
@@ -17,6 +17,7 @@ class CustomersResource extends JsonResource
         return [
             'uuid' => $this->uuid,
             'name'  => $this->name,
+            'email' => $this->email,
         ];
     }
 }
