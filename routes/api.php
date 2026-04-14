1aad41c6aac1b58c531ce00af6c12dab4c356d88
AnnXera <solivarogiann@gmail.com>
2026-04-14 14:37:49 +0800

    Final Code

--- routes/api.php
+++ routes/api.php
@@ -2,24 +2,15 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CompanyController;
-use App\Http\Controllers\LanguageController;
+use App\Http\Controllers\CustomerController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login']);
 
-
-
-
-
 Route::middleware('auth:sanctum')->group(function () {
-    // Route::delete('/logout', [AuthController::class, 'logout']);
-
-    // Route::apiResources([
-    //     'companies' => CompanyController::class,
-    //     'languages' => LanguageController::class,
-    // ]);
+    Route::delete('/logout', [AuthController::class, 'logout']);
 
     Route::apiResources([
-        // 'customers' => CustController::class
+        'customers' => CustomerController::class
     ]);
 });
