22dae63acdd2094e809ca1778b13e14f0e397107
JLAdlaw <j.adlao20@gmail.com>
2026-04-14 15:43:29 +0800

    Finished

---
+++ app/Http/Controllers/CustomerController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\CustomerStoreRequest;
+use App\Service\CustomerService;
+use Illuminate\Http\Request;
+
+class CustomerController extends Controller
+{
+    private CustomerService $customerService;
+
+    public function __construct(CustomerService $customerService)
+    {
+        $this->customerService = $customerService;
+    }
+
+    public function index()
+    {
+        return $this->customerService->listCustomer(request()->input('per_page', 15));
+    }
+
+    public function store(CustomerStoreRequest $request)
+    {
+        return $this->customerService->createCustomer($request->all());
+    }
+
+    public function show(string $uuid)
+    {
+        return $this->customerService->getCustomer($uuid);
+    }
+
+    public function update(Request $request, string $uuid)
+    {
+        return $this->customerService->updateCustomer($uuid, $request->all());
+    }
+
+    public function destroy(string $uuid)
+    {
+        $this->customerService->deleteCustomer($uuid);
+
+        return response()->json(['message' => 'Deleted successfully'], 200);
+    }
+
+    public function restore(string $uuid)
+    {
+        return $this->customerService->restoreCustomer($uuid);
+    }
+}
---
+++ app/Service/CustomerService.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Service;
+
+use App\Http\Resources\CustomerResource;
+use App\Repository\CustomerRepository;
+
+class CustomerService
+{
+    private CustomerRepository $customerRepository;
+
+    public function __construct(CustomerRepository $customerRepository)
+    {
+        $this->customerRepository = $customerRepository;
+    }
+
+    public function listcustomer(int $perPage = 15)
+    {
+        $collection = $this->customerRepository->paginate($perPage);
+
+        return CustomerResource::collection($collection);
+    }
+
+    public function createCustomer(array $payload)
+    {
+        $model = $this->customerRepository->create($payload);
+
+        return new CustomerResource($model);
+    }
+
+    public function getCustomer(string $uuid)
+    {
+        $model = $this->customerRepository->findByUuid($uuid);
+
+        return new CustomerResource($model);
+    }
+
+    public function getCustomerByField(string $field, $value)
+    {
+        $model = $this->customerRepository->findByField($field, $value);
+
+        return new CustomerResource($model);
+    }
+
+    public function updateCustomer(string $uuid, array $payload)
+    {
+        $model = $this->customerRepository->update($uuid, $payload);
+
+        return new CustomerResource($model);
+    }
+
+    public function deletecustomer(string $uuid)
+    {
+        $this->customerRepository->delete($uuid);
+
+        return true;
+    }
+
+    public function restoreCustomer(string $uuid)
+    {
+        $model = $this->customerRepository->restore($uuid);
+
+        return new CustomerResource($model);
+    }
+}
