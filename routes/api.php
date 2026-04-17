2915b675f06b01978dcfd38809e3adf988b8515f
ozius22 <lsuizo72@gmail.com>
2026-04-17 22:04:30 +0800

    Clean code

--- routes/api.php
+++ routes/api.php
@@ -15,20 +15,3 @@ Route::middleware('auth:sanctum')->group(function () {
         'languages' => LanguageController::class,
     ]);
 });
-
-Route::get('/student-profile', function () {
-    return response()->json([
-        "student_id" => "02000354357",
-        "name" => "Joshua Wayman A. Arabejo",
-        "location" => [
-            "barangay" => "3-A",
-            "city" => "Davao City"
-        ],
-        "favorites" => [
-            "movie" => "Peninsula",
-            "song" => "Martyr Nyebera",
-            "artist" => "KAMIKAZE"
-        ],
-        "is_active" => true
-    ], 200, [], JSON_PRETTY_PRINT);
-});
