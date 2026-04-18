7ca07359db668c2eaf96ffff1ef1d520ee50777e
aguilonclareynz-cell <aguilonclareynz@gmail.com>
2026-04-18 12:17:34 +0800

    Unsa

--- routes/api.php
+++ routes/api.php
@@ -3,15 +3,19 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CompanyController;
 use App\Http\Controllers\LanguageController;
+use App\Http\Controllers\CustomerController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::delete('/logout', [AuthController::class, 'logout']);
+    // Route::delete('/logout', [AuthController::class, 'logout']);
 
+    // Route::apiResources([
+    //     'companies' => CompanyController::class,
+    //     'languages' => LanguageController::class,
+    // ]);
     Route::apiResources([
-        'companies' => CompanyController::class,
-        'languages' => LanguageController::class,
+        'customers' => CustomerController::class
     ]);
 });
