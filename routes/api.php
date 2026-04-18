719b480669c1af01f0bbc69fd037eb8590741e5f
iamrinalin <renalynbulotano8@gmail.com>
2026-04-18 12:12:03 +0800

    in progress

--- routes/api.php
+++ routes/api.php
@@ -3,15 +3,15 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CompanyController;
 use App\Http\Controllers\LanguageController;
+use App\Http\Controllers\CustomerController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::delete('/logout', [AuthController::class, 'logout']);
+    // Route::delete('/logout', [AuthController::class, 'logout']);
 
     Route::apiResources([
-        'companies' => CompanyController::class,
-        'languages' => LanguageController::class,
+        'customer' => CustomerController::class
     ]);
 });
