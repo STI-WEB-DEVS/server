f6dc68778e38545f750e1ceca327e71ee9b49d03
Vixzyyy <rafaelababa9@gmail.com>
2026-04-18 12:01:42 +0800

    restful api

---
+++ app/Http/Controllers/CustomerController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Http\Requests\CustomerStoreRequest;
+use Illuminate\Http\Request;
+use App\Service\CustomerService;
+
+class CustomerController extends Controller
+{
+
+    private CustomerService $customerService;
+
+    public function __construct(CustomerService $customerService){
+        $this->customerService = $customerService;
+    }
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+        return $this->customerService->paguinate();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(CustomerStoreRequest $request)
+    {
+        //
+       return $this->customerService->createCustomer($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+       return $this->customerService->getCustomer($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(CustomerStoreRequest $request, string $id)
+    {
+        //
+
+        return $this->customerService->updateCustomer($request->all(), $id);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+
+        return $this->customerService->deletCustomer($id);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CompanyController;
 use App\Http\Controllers\LanguageController;
+use App\Http\Controllers\CustomerController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login']);
@@ -12,14 +13,14 @@ Route::post('/login', [AuthController::class, 'login']);
 
 
 Route::middleware('auth:sanctum')->group(function () {
-    // Route::delete('/logout', [AuthController::class, 'logout']);
-
-    // Route::apiResources([
-    //     'companies' => CompanyController::class,
-    //     'languages' => LanguageController::class,
-    // ]);
+    Route::delete('/logout', [AuthController::class, 'logout']);
 
     Route::apiResources([
-        // 'customers' => CustController::class
+        'companies' => CompanyController::class,
+        'languages' => LanguageController::class,
+    ]);
+
+    Route::apiResources([
+        'customers' => CustomerController::class
     ]);
 });
