414672e84d1cf66ca8fade19e71b320f4de6e24a
marlodeang22 <marlodeang222@gmail.com>
2026-04-21 22:43:56 +0800

    Complete Order

--- routes/api.php
+++ routes/api.php
@@ -3,23 +3,20 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CompanyController;
 use App\Http\Controllers\LanguageController;
+use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\OrderController;
+
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login']);
 
 
-
-
-
 Route::middleware('auth:sanctum')->group(function () {
-    // Route::delete('/logout', [AuthController::class, 'logout']);
-
-    // Route::apiResources([
-    //     'companies' => CompanyController::class,
-    //     'languages' => LanguageController::class,
-    // ]);
 
     Route::apiResources([
-        // 'customers' => CustController::class
+        'orders' => OrderController::class,
+        'products' => ProductController::class,
+        'customers' => CustomerController::class,
     ]);
 });
