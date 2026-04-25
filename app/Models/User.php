c38f8d9e046a096b6aad930928e849d15fece76f
Carl Joseph Orence <Umbay84@gmail.com>
2026-04-25 17:16:50 +0800

    feat: Added roles

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,11 +6,12 @@ use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+use Laravel\Sanctum\HasApiTokens; 
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, HasUuids, Notifiable;
+    use HasApiTokens, HasFactory, HasUuids, Notifiable, HasRoles;
 
     public function uniqueIds()
     {
@@ -22,7 +23,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
-        'uuid',
+        'customer_id',
     ];
 
     protected $hidden = [
