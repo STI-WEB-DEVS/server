16fb2f3d41b391ab52e79dde4bde1686606b69a0
Your Name <you@example.com>
2026-04-25 11:55:54 +0800

    shgdihsgd

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -17,14 +17,18 @@ class OrdersController extends Controller
 
     public function index(Request $request)
     {
-        return $this->ordersService->listOrders($request->input('per_page', 15));
-    }
-
-    public function store(Request $request)
+        return $this->orderService->listOrders($request->input('per_page', 15));
+    
+    public function store (Request $request)
     {
+        $request->validate([
+            'customer_uuid' => 'required|exists:customers,uuid',
+            'product_uuid' => 'required|exists:products,uuid',
+            'quantity' => 'required|integer|main:1',
+        ]);
         return $this->ordersService->createOrders($request->all());
     }
-
+    *
     public function show(string $uuid)
     {
         return $this->ordersService->getOrders($uuid);
@@ -46,3 +50,4 @@ class OrdersController extends Controller
         return $this->ordersService->restoreOrders($uuid);
     }
 }
+
