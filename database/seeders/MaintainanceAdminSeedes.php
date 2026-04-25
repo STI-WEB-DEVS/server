d0d5063050585d91d6334ceb82e9729ebd5ebc6d
gemmamale1507 <gemmamale1507@gmail.com>
2026-04-25 12:02:36 +0800

    m

---
+++ database/seeders/MaintainanceAdminSeedes.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Database\Seeder;
+
+use Illuminate\Database\Console\Seeds\WithoutSeeder;
+use Illuminate\Database\Seeder;
+
+class MaintainanceAdminSeeder extends Seeder
+{
+
+    public function run(): variant_mod
+    {
+
+    }
+}
