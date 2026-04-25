13b20ea776890cc9b36370a72df91ec873556985
Kir205 <rafaeljoshsanchez11@gmail.com>
2026-04-25 18:01:42 +0800

    done

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -6,14 +6,21 @@ use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class UserResource extends JsonResource
-{
-    /**
-     * Transform the resource into an array.
-     *
-     * @return array<string, mixed>
-     */
-    public function toArray(Request $request): array
-    {
-        return parent::toArray($request);
+{00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000..............
+    public function toArray(Request$request):array{
+        return[
+            'id' => $this->id,
+            'uuid' => $this->uuid,
+            'company_id' => $this-> company_id,
+            'customer_id' => $this-> customer_id,
+            'name' => $this-> name,
+            'email' => $this-> email,
+            'email_verified_at' =>$this-> email_verified_at,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+            'role' => $this->getRoleNames()->first(),
+
+
+        ];
     }
 }
