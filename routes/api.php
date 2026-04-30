2b829d7d9734d1950aa4cbce109274b366bf43bc
Nicolaiixx <etnegaled14@gmail.com>
2026-04-30 17:44:03 +0800

    commit

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,8 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CompanyController;
 use App\Http\Controllers\LanguageController;
 use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login']);
@@ -22,7 +24,7 @@ Route::middleware('auth:sanctum')->group(function () {
 
     Route::apiResources([
         'customers' => CustomerController::class,
-        'products' => ProductsController::class,
+        'products' => ProductController::class,
         'orders' => OrderController::class
     ]);
 
