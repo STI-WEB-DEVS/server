c3a2c5980239f6ed40722659af51a2a26918c97a
marlodeang22 <marlodeang222@gmail.com>
2026-05-26 15:58:59 +0800

    Not working order

--- routes/api.php
+++ routes/api.php
@@ -11,7 +11,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login']);
 
-
 Route::middleware('auth:sanctum')->group(function () {
 
     Route::apiResources([
