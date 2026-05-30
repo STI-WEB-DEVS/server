30d6ff4b0288b877eb8049725e366872a8f6ff35
Charlie <aikoelagout@gmail.com>
2026-05-30 17:33:24 +0800

    Stocks Added

--- app/Http/Requests/ProductStoreRequest.php
+++ app/Http/Requests/ProductStoreRequest.php
@@ -12,10 +12,22 @@ class ProductStoreRequest extends FormRequest
     }
 
     public function rules(): array
-    {
-        return [
-            'name'  => ['required', 'string', 'max:255'],
-            'price' => ['required', 'numeric', 'min:0'],
-        ];
-    }
+{
+    return [
+        'name'        => 'required|string|max:255|regex:/[a-zA-Z]/',
+        'description' => 'nullable|string',
+        'price'       => 'required|numeric|min:0',
+        'stocks'      => 'required|integer|min:0',
+    ];
+}
+
+public function messages(): array
+{
+    return [
+        'name.regex'    => 'Product name must contain at least one letter.',
+        'stocks.integer' => 'Stocks must be a whole number.',
+        'price.numeric' => 'Price must be a valid number.',
+    ];
+}
+
 }
