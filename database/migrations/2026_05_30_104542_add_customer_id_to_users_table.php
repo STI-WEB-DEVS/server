cd3327dc6747432640f7a83ca98936a4e7c1bd9e
hededbry <dedrichbry@gmail.com>
2026-05-30 19:34:39 +0800

    job's not done

--- database/migrations/2026_05_30_104542_add_customer_id_to_users_table.php
+++ database/migrations/2026_05_30_104542_add_customer_id_to_users_table.php
@@ -1,8 +1,9 @@
-    <?php
+<?php
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+
 
 return new class extends Migration
 {
@@ -12,19 +13,21 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->foreignId('company_id')
-                ->nullable()
-                ->after('id')
-                ->constrained('companies')
-                ->nullOnDelete();
+        $table->foreignId('customer_id')
+        ->nullable()
+        ->constrained('customers')
+        ->nullOnDelete();
         });
     }
 
+
+    /**
+     * Reverse the migrations.
+     */
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropForeign(['company_id']);
-            $table->dropColumn('company_id');
+            //
         });
     }
 };
